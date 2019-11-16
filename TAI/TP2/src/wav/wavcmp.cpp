dca4f757ec42e0ddd39354003f2f03c65993a91f
lengors <pedrocavadinhas@gmail.com>
2019-11-16 14:00:47 +0000

    TAI

--- TAI/TP2/src/wav/wavcmp.cpp
+++ TAI/TP2/src/wav/wavcmp.cpp
@@ -28,5 +28,5 @@ std::optional<std::tuple<float, unsigned>> WAV::compare(SndfileHandle &file, Snd
             diff_sum += diff * diff;
         }
     }
-    return std::optional({ float(10 * std::log10(sum / diff_sum)), unsigned(max) });
+    return std::optional<std::tuple<float, unsigned>>({ float(10 * std::log10(sum / diff_sum)), unsigned(max) });
 }
