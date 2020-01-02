b3d9230b210489e382244f2d2702c1af570d0b41
Lucas Barros <lucasfrb45@ua.pt>
2020-01-02 15:47:54 +0000

    update

--- AA/TP3/bloom_filter.py
+++ AA/TP3/bloom_filter.py
@@ -1,12 +1,15 @@
 import random
-from math import sqrt
+from math import sqrt, ceil
 
 class BloomFilter:
 
-    def __init__(self, rel = 8,  k = 5):      # k = num de hash functions
+    def __init__(self, rel = 8,  k = 5):      # k = num de hash functions, rel relação entre numero de palavras e tamanho do filtro
         self.rel = rel
         self.k = k
-        self.primes = [ self.__gen_prime(100, 500) for i in range(self.k) ]
+        self.primes = list()
+        for i in range(2 * self.k):
+            self.primes.append(self.__gen_prime(0, 50, self.primes))
+        
 
 
     def extend(self, lista):
@@ -50,20 +53,23 @@ class BloomFilter:
                 return False
         return True
 
-    def __gen_prime(self, min_range, max_range):
+    def __gen_prime(self, min_range, max_range, non_options = list()):
         lista = list()
         for i in range(min_range, max_range):
-            if self.__is_prime(i):
+            if self.__is_prime(i) and not i in non_options:
                 lista.append(i)
 
         return random.choice(lista)
 
     def __hashf(self, string, index):
-        soma = 0
+        soma = self.primes[index]
         for pos in string:
-            soma += ord(pos) * self.primes[index]
+            soma = soma * self.primes[self.k + index] * ord(pos)
 
         return soma % self.m
 
     def __repr__(self):
         return "n = " + str(self.n) + " ; f = " + str(self.f)
+
+    def __len__(self):
+        return ceil( self.bf.count(1) / self.k)
--- AA/TP3/main.py
+++ AA/TP3/main.py
@@ -1,19 +1,34 @@
 import re
 from bloom_filter import BloomFilter
+import os
 
 def read_file(filename):
     l = list()
     with open(filename, encoding = 'UTF-8') as file:
         for line in file:               # podemos usar o tokenizer de RI maybe
-            lista = re.split('; |, |\*|\n|\s|\.',line.strip())  
-            l.extend(lista)
+            if line != '':
+                lista = re.split('; |, |\*|\n|\s|\.',line.strip())  
+                l.extend(lista)
 
     return l
 
 
 if __name__ == "__main__":
-    words = read_file("Maias.txt")
-    bf = BloomFilter()
-    bf.extend(words)
-    print(str(bf))
-    print(bf.contains("Maia"))
+
+
+    rel_sizes = [2, 3, 5, 7, 8, 10, 12, 15, 20, 25, 30, 35, 40, 45, 50]
+    for book in os.listdir("books"):
+        words = read_file(os.path.join("books", book))
+        diff_words = list(set(words))
+        print("Livro : {}".format(book))
+        print("Número de palavras distintas : {}".format(len(diff_words)))
+        
+
+        for size in rel_sizes:
+            bf = BloomFilter(rel = size)
+            bf.extend(words)
+            print("rel {}x - {}".format(size, len(bf)))
+        print("")
+    
+
+    
